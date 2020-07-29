c9f4922a7ed0114afc6c8112b8a22c02dc295c29
dmmaia <david_mmaia@hotmail.com>
2020-07-29 13:28:57 -0300

    improving field generation

--- src/src/MineField.js
+++ src/src/MineField.js
@@ -6,84 +6,12 @@ export default function MineField() {
 
   const [values, setValues] = useState([0]);
 
-  useEffect(() => {
-    function defineValue() {
-      for (let i = 0; i < 24; i++) {
-      let individualValue = Math.random() * (4 - 1) + 1;
-      setValues([...values, parseInt(individualValue)]);
-      console.log(values)
-    }
-  }
-  defineValue();
-    //eslint-disable-next-line
-  }, [])
-
   return (
     <div className="mineField" >
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
-      <MineBlock />
+      {
+        [...Array(64)].map((x, i) =>
+        <MineBlock />
+        )}
     </div>
   );
 }
